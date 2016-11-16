85d273b65ff6ec6c22594bb89feefaba68d8ddeb
Shanthi robinson <shanthi.robinson100@gmail.com>
2016-11-16 11:30:54 -0800

    Made food move

--- GameProject.js
+++ GameProject.js
@@ -1,23 +1,32 @@
 var x = 0
 var y = 0
+
 document.addEventListener("keydown", function(e) {
  if (e.keyCode == 37) {
-   document.getElementById("bat").setAttribute("x", x - 15)
+   document.getElementById("IronMan").setAttribute("x", x - 15)
   x = x - 15
 }
 
 else if (e.keyCode == 39) {
-   document.getElementById("bat").setAttribute("x", x + 15)
+   document.getElementById("IronMan").setAttribute("x", x + 15)
   x = x + 15
 }
 
 else if (e.keyCode == 38) {
-   document.getElementById("bat").setAttribute("y", y - 15)
+   document.getElementById("IronMan").setAttribute("y", y - 15)
   y = y - 15
 }
 
 else if (e.keyCode == 40) {
-   document.getElementById("bat").setAttribute("y", y + 15)
+   document.getElementById("IronMan").setAttribute("y", y + 15)
   y = y + 15
 }
+
+var capsX = Number(document.getElementById("CapsShield").getAttribute("x"));
+var capsY = Number(document.getElementById("CapsShield").getAttribute("y"));
+
+if(x>capx && x<capx+capwidth && y>capy && y<capy+capwidth){
+  
+}
+
   })
