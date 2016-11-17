ba01582e9c2788f223ff15e232f6628c8118af27
Shanthi robinson <shanthi.robinson100@gmail.com>
2016-11-17 11:31:28 -0800

    Player collides with food on x-axis

--- GameProject.js
+++ GameProject.js
@@ -1,32 +1,39 @@
-var x = 0
-var y = 0
+var player = document.getElementById("player")
+var food1 = document.getElementById("food1")
+var x = Number(player.getAttribute("x"))
+var y = Number(player.getAttribute("y"))
+var foodX = Number(food1.getAttribute("x"))
+var foodY = Number(food1.getAttribute("y"))
+var foodWidth = 40;
+var foodHeight = 40;
 
 document.addEventListener("keydown", function(e) {
- if (e.keyCode == 37) {
-   document.getElementById("IronMan").setAttribute("x", x - 15)
-  x = x - 15
-}
+    if (e.keyCode == 37) {
+        document.getElementById("player").setAttribute("x", x - 15)
+        x = x - 15
+    } else if (e.keyCode == 39) {
+        document.getElementById("player").setAttribute("x", x + 15)
+        x = x + 15
+    } else if (e.keyCode == 38) {
+        document.getElementById("player").setAttribute("y", y - 15)
+        y = y - 15
+    } else if (e.keyCode == 40) {
+        document.getElementById("player").setAttribute("y", y + 15)
+        y = y + 15
+    }
 
-else if (e.keyCode == 39) {
-   document.getElementById("IronMan").setAttribute("x", x + 15)
-  x = x + 15
-}
+    if (x > foodX && x < foodX + foodWidth ){//&& y + 50 > foodY && y + 50 < foodY + foodHeight) {
+      if(Number(food1.getAttribute("x") == 250)) {
+        food1.setAttribute("x", 100);
+      }
+      else {
+      food1.setAttribute("x", 250);
+      }
+        console.log("collide");
+    }
+    console.log(x + "<" + (foodX + foodWidth) +"  " + x + ">" + foodX);
+        /*if( x > capsX && x < capsX + capsWidth && y > capsY && y < capsY + capsWidth){
+          console.log("collide");
+        }*/
 
-else if (e.keyCode == 38) {
-   document.getElementById("IronMan").setAttribute("y", y - 15)
-  y = y - 15
-}
-
-else if (e.keyCode == 40) {
-   document.getElementById("IronMan").setAttribute("y", y + 15)
-  y = y + 15
-}
-
-var capsX = Number(document.getElementById("CapsShield").getAttribute("x"));
-var capsY = Number(document.getElementById("CapsShield").getAttribute("y"));
-
-if(x>capx && x<capx+capwidth && y>capy && y<capy+capwidth){
-  
-}
-
-  })
+})
