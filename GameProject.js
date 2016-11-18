892dfaca76f4123966053dd58bb147a25f0df018
Shanthi robinson <shanthi.robinson100@gmail.com>
2016-11-18 11:20:43 -0800

    I GOT COLLIDE TO WORK

--- GameProject.js
+++ GameProject.js
@@ -1,28 +1,49 @@
-var player = document.getElementById("player")
-var food1 = document.getElementById("food1")
-var x = Number(player.getAttribute("x"))
-var y = Number(player.getAttribute("y"))
-var foodX = Number(food1.getAttribute("x"))
-var foodY = Number(food1.getAttribute("y"))
-var foodWidth = 40;
-var foodHeight = 40;
+
+function randomNumber(min,max){
+ return Math.floor(Math.random()*(max-min+1)+min);
+}
 
 document.addEventListener("keydown", function(e) {
+  var player = document.getElementById("player")
+  var food1 = document.getElementById("food1")
+  var x = Number(player.getAttribute("x"))
+  var y = Number(player.getAttribute("y"))
+  var foodX = Number(food1.getAttribute("x"))
+  var foodY = Number(food1.getAttribute("y"))
+  var foodWidth = 40;
+  var foodHeight = 40;
+
     if (e.keyCode == 37) {
-        document.getElementById("player").setAttribute("x", x - 15)
+      if (x > foodX && x < foodX + foodWidth && y > foodY && y < foodY + foodWidth){
+        food1.setAttribute("x", randomNumber(40,810))
+        console.log("touch")
+      }
+        player.setAttribute("x", x - 15)
         x = x - 15
     } else if (e.keyCode == 39) {
-        document.getElementById("player").setAttribute("x", x + 15)
+      if (x > foodX && x < foodX + foodWidth && y > foodY && y < foodY + foodWidth){
+        food1.setAttribute("x", randomNumber(40,810))
+        console.log("touch")
+      }
+      player.setAttribute("x", x + 15)
         x = x + 15
     } else if (e.keyCode == 38) {
-        document.getElementById("player").setAttribute("y", y - 15)
+      if (x > foodX && x < foodX + foodWidth && y > foodY && y < foodY + foodWidth){
+          food1.setAttribute("x", randomNumber(40,810))
+        console.log("touch")
+      }
+        player.setAttribute("y", y - 15)
         y = y - 15
     } else if (e.keyCode == 40) {
-        document.getElementById("player").setAttribute("y", y + 15)
+      if (x > foodX && x < foodX + foodWidth && y > foodY && y < foodY + foodWidth){
+          food1.setAttribute("x", randomNumber(40,810))
+        console.log("touch")
+      }
+      player.setAttribute("y", y + 15)
         y = y + 15
     }
-
-    if (x > foodX && x < foodX + foodWidth ){//&& y + 50 > foodY && y + 50 < foodY + foodHeight) {
+/*
+    //&& y + 50 > foodY && y + 50 < foodY + foodHeight) {
       if(Number(food1.getAttribute("x") == 250)) {
         food1.setAttribute("x", 100);
       }
@@ -31,7 +52,7 @@ document.addEventListener("keydown", function(e) {
       }
         console.log("collide");
     }
-    console.log(x + "<" + (foodX + foodWidth) +"  " + x + ">" + foodX);
+    //console.log(x + "<" + (foodX + foodWidth) +"  " + x + ">" + foodX);
         /*if( x > capsX && x < capsX + capsWidth && y > capsY && y < capsY + capsWidth){
           console.log("collide");
         }*/
